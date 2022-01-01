348a36f7b17c780c3c559e98eb56e25d4fc15fa0
Firose Hussain <github@firumon.com>
2022-01-01 13:05:43 +0530

    Initialization

--- dev/quasar.conf.js
+++ dev/quasar.conf.js
@@ -69,7 +69,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 8081,
+      port: 8080,
       before (app) {
         const cors = require('cors')
         app.use(cors())
